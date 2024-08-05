2bd51b08478c06924d54fb216fd731e8d7292bb2
Sarah Gu <guwen888@gmail.com>
2024-08-05 16:29:39 -0400

    Hobbies - Added photos

--- app/tabs/Hobbies.tsx
+++ app/tabs/Hobbies.tsx
@@ -29,33 +29,49 @@ export const Hobbies = () => {
         <div
           className="w-2/5 h-full justify-center items-center flex relative"
           style={{
-            backgroundImage: "url('/camino.png')",
+            backgroundImage: "url('/restaurants.png')",
             backgroundSize: "cover",
           }}
-        ></div>
+        >
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-30 text-white flex justify-center items-center hover:bg-opacity-50">
+            Restaurants
+          </div>
+        </div>
         <div className="flex flex-col gap-1 w-1/5 h-full">
           <div
-            className="h-1/2 w-full"
+            className="h-1/2 w-full justify-center items-center flex relative "
             style={{
-              backgroundImage: "url('/camino.png')",
+              backgroundImage: "url('/climbing.png')",
               backgroundSize: "cover",
             }}
-          ></div>
+          >
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-30 text-white flex justify-center items-center hover:bg-opacity-50">
+              Climbing
+            </div>
+          </div>
           <div
-            className="h-1/2 w-full"
+            className="h-1/2 w-full justify-center items-center flex relative"
             style={{
-              backgroundImage: "url('/camino.png')",
+              backgroundImage: "url('/biking.png')",
               backgroundSize: "cover",
             }}
-          ></div>
+          >
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-30 text-white flex justify-center items-center hover:bg-opacity-50">
+              Biking
+            </div>
+          </div>
         </div>
         <div
           className="w-2/5 h-full justify-center items-center flex relative"
           style={{
-            backgroundImage: "url('/camino.png')",
+            backgroundImage: "url('/elephant.png')",
             backgroundSize: "cover",
           }}
-        ></div>
+        >
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-30 text-white flex justify-center items-center hover:bg-opacity-50">
+            Travel
+          </div>
+        </div>
       </div>
     </div>
   );
